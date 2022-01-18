fdbde176ed86439ea3092d7b0dd3f78a6463137b
schm00g <hk8igureu@relay.firefox.com>
2022-01-18 20:24:11 +0000

    JSX returns plain JS Objects

--- index.js
+++ index.js
@@ -1,16 +1,11 @@
-const navbar = (
-    // this is JSX i.e. HTML in JS
-    <nav>
-        <h1>Places to visit</h1>
-        <ol>
-            <li><a href="https://www.google.com/search?q=WTF+is+JSX">Place A</a></li>
-            <li><a href="https://www.google.com/search?q=WTF+is+JSX">Place B</a></li>
-            <li><a href="https://www.google.com/search?q=WTF+is+JSX">Place C</a></li>
-        </ol>
-    </nav>
-)
+// import React from "react";
 
-ReactDOM.render(
-    navbar,
-    document.getElementById("root")
+// JSX returns plain JS Objects
+const page = (
+    <div>
+        <p>I</p>
+        <p>Was</p>
+        <p>Here</p>
+    </div>
 )
+document.getElementById("root").append(JSON.stringify(page)); // [object Object]
